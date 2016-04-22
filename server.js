b581cb0dbbb31172fc451ca4f4e1ebe4a5d864f0
alan <alankn125@gmail.com>
2016-04-21 18:58:33 -0700

    add jade file, fix jade file settings, add reroute to index page

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-app.configure(function () {
-    app.set('views', __dirname + '/server/views');
-    app.set('view engine', 'jade');
+var env = app.settings.env;
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+
+
+
+// gets route for index page
+// asterisk will match all route, delivers index page whenever client ask for request that don't exist
+app.get('*', function(req, res) {
+    res.render('index');
 });
 
-app.get('*', function(req, res) {
-    res.sender('index');
-});
+// tells app to listen for requests
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
